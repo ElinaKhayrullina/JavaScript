9a9e357b195fbfb0461d2e3c1b4f174080485ad1
Diana Bayazitova <onlydi@mail.ru>
2022-11-22 21:21:56 +0500

    Initial commit

---
+++ 1.js
@@ -0,0 +1,5 @@
+"user strict"
+const temp=Number.parseInt(prompt("Input temperature in your town (C)"));
+const fahr=parseInt((9 / 5) * temp + 32);
+alert(` Celsius: ${temp}, Fahrenheit: ${fahr} `);
+
---
+++ 2.js
@@ -0,0 +1,7 @@
+"user strict"
+const temp=prompt("Input your name");
+printName(temp);
+function printName (name){
+    alert(`Hello, ${name} `);
+}
+
